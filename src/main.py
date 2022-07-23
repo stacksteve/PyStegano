b9b1ff0aac49d8ab204548659db3a2ea36bf9a8b
Steve Hamann <s.hamann@tu-bs.de>
2022-07-23 07:58:48 +0200

    - Example how to use in main.py

--- src/main.py
+++ src/main.py
@@ -1,12 +1,16 @@
-from Stegano import SteganoWriter, SteganoReader, Stegano
+from Stegano import SteganoWriter, SteganoReader
 
 
 def main():
+    # Place message
     steg = SteganoWriter("steg.png", "stegged.png")
-    steg.placeSecretMessage("H")
+    secret_message = str(input("Secret message: "))
+    steg.placeSecretMessage(secret_message)
+
+    # Extract message
     stegged = SteganoReader("steg.png", "stegged.png")
     stegged.extractSecretMessage()
-    print(stegged.extracted_message)
+    print("Extracted message:", stegged.extracted_message)
 
 
 if __name__ == "__main__":
