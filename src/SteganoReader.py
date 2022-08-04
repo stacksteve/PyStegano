08498186e90be2c64a0e772a0ee777c11d2adcd2
Steve Hamann <s.hamann@tu-bs.de>
2022-08-04 19:02:35 +0200

    - Code cleanup

--- src/SteganoReader.py
+++ src/SteganoReader.py
@@ -14,32 +14,29 @@ class SteganoReader(Stegano):
         return self.__extracted_message
 
     def __findSeperatorPosition(self) -> int:
-        seperator_begin = -1
         temp_string = ""
         i = 0
-        while seperator_begin == -1:
-            temp_string += str(self.__bitWasFlipped(i))
-            if i % 136 == 0 and i >= self.seperator_length:     # see performanceTest.py
+        while True:
+            temp_string += self.__extractBitAt(i)
+            if i >= self.seperator_length and i % 136 == 0:  # Reason for % 136 see performanceTest.py
                 seperator_position = temp_string.find(self.seperator_binary)
                 if seperator_position != -1:
-                    seperator_begin = seperator_position
+                    return seperator_position
             i += 1
-        return seperator_begin
 
     def __extractSecretMessageLength(self) -> tuple:
         seperator_begin = self.__findSeperatorPosition()
         assert seperator_begin != -1
         secret_message_length = ""
         for i in range(seperator_begin):
-            secret_message_length += str(self.__bitWasFlipped(i))
+            secret_message_length += self.__extractBitAt(i)
         return int(self.binaryToString(secret_message_length)), seperator_begin + self.seperator_length
 
     def extractSecretMessage(self):
-        secret_message_bits = ""
         secret_message_length, seperator_end = self.__extractSecretMessageLength()
-        for i in range(seperator_end, seperator_end + secret_message_length):
-            secret_message_bits += str(self.__bitWasFlipped(i))
-        self.__extracted_message = self.binaryToString(secret_message_bits)
+        secret_message_end = seperator_end + secret_message_length
+        secret_message = "".join([self.__extractBitAt(i) for i in range(seperator_end, secret_message_end)])
+        self.__extracted_message = self.binaryToString(secret_message)
 
-    def __bitWasFlipped(self, i: int) -> int:
-        return self.__original_image_data[i][0] ^ self.__stegano_image_data[i][0]
+    def __extractBitAt(self, i: int) -> str:
+        return str(self.__original_image_data[i][0] ^ self.__stegano_image_data[i][0])
