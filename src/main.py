8703c64580c6e2989437285a7df0b5dd8848865d
Steve Hamann <s.hamann@tu-bs.de>
2022-08-04 08:12:14 +0200

    - Example for main.py

--- src/main.py
+++ src/main.py
@@ -1,14 +1,14 @@
-from Stegano import SteganoWriter, SteganoReader
+from Stegano import SteganoWriter, SteganoReader, Stegano
 
 
 def main():
     # Place message
-    steg = SteganoWriter("steg.png", "stegged.png")
+    steg = SteganoWriter("desktop.png", "desktop_secret.png")
     secret_message = str(input("Secret message: "))
     steg.placeSecretMessage(secret_message)
 
     # Extract message
-    stegged = SteganoReader("steg.png", "stegged.png")
+    stegged = SteganoReader("desktop.png", "desktop_secret.png")
     stegged.extractSecretMessage()
     print("Extracted message:", stegged.extracted_message)
 
