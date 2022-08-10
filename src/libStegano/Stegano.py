43e7bbb1d2c42886195b1062cc5ce14eb03ab980
Steve Hamann <s.hamann@tu-braunschweig.de>
2022-08-10 17:58:10 +0200

    - Code cleanup - Doc comments in Stegano.py - Implemented core functionality of createSeperator in Stegano.py

--- src/libStegano/Stegano.py
+++ src/libStegano/Stegano.py
@@ -1,22 +1,45 @@
 class Stegano:
     def __init__(self):
+        # self.seperator = getSeperator(message) -> should look like this later
         self.seperator = "!#!SepSepSep!#!"
         self.seperator_binary = self.stringToBinary(self.seperator)
         self.seperator_length = len(self.seperator_binary)
 
     @staticmethod
-    def stringToBinary(string: str) -> str:
-        return "".join([format(ord(char), "08b") for char in string])
+    def stringToBinary(message: str) -> str:
+        """
+        Get bytes from message string.
+
+        :param message: The original message, than should be placed inside an image.
+        :return: The binary representation of the
+        """
+        return "".join([format(ord(char), "08b") for char in message])
 
     @staticmethod
-    def binaryToString(binary: str) -> str:
-        # Evaluate bit string bytewise
-        return "".join(chr(int(binary[i * 8:i * 8 + 8], 2)) for i in range(len(binary) // 8))
+    def binaryToString(binary_message: str) -> str:
+        """
+        Evaluate bit string bytewise.
+
+        :param binary_message: Binary message that has been extracted from an image.
+        :return: The string representation of the bitstream.
+        """
+        return "".join(chr(int(binary_message[i * 8:i * 8 + 8], 2)) for i in range(len(binary_message) // 8))
 
     @staticmethod
     def intToBinary(integer: int) -> str:
-        digits = []
-        while integer > 0:
-            digits.insert(0, integer % 10)
-            integer //= 10
-        return "".join(format(digit + 48, "08b") for digit in digits)  # 48 to get char-value of '5'
+        if integer > 255:
+            raise ValueError("Only values less or equal 255 (1 byte) are allowed")
+        return format(integer, "08b")
+
+    @staticmethod
+    def binaryToInt(binary: str) -> int:
+        return int(binary, 2)
+
+    # TODO: Create seperator by evaluating the input
+    @staticmethod
+    def createSeperator(message: str) -> str:
+        special_characters = set('.:,;-!?#@&%$*+<>=()')
+        print(special_characters)
+        for character in special_characters:
+            if character not in message:
+                return character
