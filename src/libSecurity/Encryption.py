c0b1a5bc12b66705f8564eb5e7d8d7419d70fd27
Steve Hamann <s.hamann@tu-bs.de>
2022-08-11 11:47:29 +0200

    - Code cleanup

--- src/libSecurity/Encryption.py
+++ src/libSecurity/Encryption.py
@@ -5,36 +5,33 @@ from Crypto.PublicKey import RSA
 from Crypto.Random import get_random_bytes
 
 
-# from Crypto.Random import get_random_bytes -> for later use with ECC encryption
-
-
-def generateKeyPair(key_name: str):
+def generateKeyPair(key_name: str) -> None:
     private_key = RSA.generate(8192)
     public_key = private_key.public_key()
     open(f"{key_name}_private.pem", "wb").write(private_key.export_key(format="PEM"))
     open(f"{key_name}_public.pem", "wb").write(public_key.export_key(format="PEM"))
 
 
-def importAsymmetricKey(path):
+def importAsymmetricKey(path) -> RSA:
     return RSA.import_key(open(path, "rb").read())
 
 
-def encryptKey(public_key_receiver, key):
+def encryptKey(public_key_receiver, key) -> bytes:
     cipher = PKCS1_OAEP.new(public_key_receiver)
     return cipher.encrypt(key)
 
 
-def decryptKey(private_key_receiver, encrypted_key):
+def decryptKey(private_key_receiver, encrypted_key) -> bytes:
     cipher = PKCS1_OAEP.new(private_key_receiver)
     return cipher.decrypt(encrypted_key)
 
 
-def getNewSymmetricEncryptionKey():
+def getNewSymmetricEncryptionKey() -> bytes:
     key = get_random_bytes(32)
     return SHA3_256.new(key).digest()
 
 
-def encryptMessage(message: str, public_key_receiver):
+def encryptMessage(message: str, public_key_receiver) -> dict:
     key = getNewSymmetricEncryptionKey()
     cipher = AES.new(key, AES.MODE_CFB)
     ciphertext_bytes = cipher.encrypt(message.encode())
@@ -45,7 +42,7 @@ def encryptMessage(message: str, public_key_receiver):
     }
 
 
-def decryptMessage(ciphertext, iv, key, private_key_receiver):
+def decryptMessage(ciphertext, iv, key, private_key_receiver) -> str:
     iv = b64decode(iv)
     ciphertext_bytes = b64decode(ciphertext)
     cipher = AES.new(decryptKey(private_key_receiver, key), AES.MODE_CFB, iv)
