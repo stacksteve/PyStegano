fa7908b603cf590a0a41b3153e2f8df23aba84a2
Steve Hamann <s.hamann@tu-bs.de>
2022-08-14 17:53:36 +0200

    - Implemented file reading in PicReader.py

--- src/utils/PicReader.py
+++ src/utils/PicReader.py
@@ -1,3 +1,31 @@
-def detectFormat(path: str):
+from enum import Enum
+from PIL import Image
+
+
+class ImageFormat(Enum):
+    PNG = 0
+    JPG = JPEG = 1
+    PPM = 2
+
+
+def detectFormat(image_path: str) -> ImageFormat:
+    image_format = image_path.split(".")[-1].upper()
+    return ImageFormat[image_format].value
+
+
+def readImageData(image_path: str) -> tuple:
+    image_rgb = Image.open(image_path).convert("RGB")
+    image_rgb_data = image_rgb.getdata()
+    return image_rgb, image_rgb_data
+
+
+def writeImage() -> None:
     pass
 
+
+def main():
+    print(detectFormat("test.png"))
+
+
+if __name__ == '__main__':
+    main()
