42d97a62bedf8565274492108639eef384ef7343
Steve Hamann <s.hamann@tu-bs.de>
2022-12-31 10:55:53 +0100

    Network (for the moment) stuff is not part of this project anymore

--- src/libSocket/Client.py
+++
@@ -1,7 +0,0 @@
-import socket
-
-
-def transmit_data(data: bytes, host: str, port: int):
-    with socket.socket() as s:
-        s.connect((host, port))
-        s.sendall(data)
--- src/libSocket/__init__.py
+++
@@ -1,2 +0,0 @@
-from libSocket.Client import transmit_data
-from libSocket.Server import receive_data
